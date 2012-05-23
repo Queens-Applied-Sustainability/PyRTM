99cc0dbe8be0c9307738f70b5d0e043b24e4de88
Philip Schleihauf <uniphil@gmail.com>
2012-05-23 05:38:07 -0400

    Start fleshing out the generic atmosphere model

--- pyrtm/rtm.py
+++ pyrtm/rtm.py
@@ -2,16 +2,26 @@
 
 The atmospheric settings here are blah blah blah
 """
+     
+#   self.description = None  # Note: SMARTS trucates at 64 chars
 
+#self.surface_pressure = None
 
-class Atmosphere:
-    def __init__(self):
-        self.description = None  # Note: SMARTS trucates at 64 chars
-        
-        #self.surface_pressure = None
-        
-        #self.altitude = None
-        #self.latitude = None
-        #self.height = None
+#self.altitude = None
+#self.latitude = None
+#self.height = None
     
-    
+   
+atmosphere = {
+    description: "Example run",                         # SMARTS
+    solar_zenith_angle: None,                           # SBDART
+    atmospheric_profile: "sub_arctic_summer",           # SBDART
+    # SBDART can handle up to 5 cloud layers
+    clouds: [
+        {
+            optical_thickness: 10
+            drop_radius: 10, # microns
+            altitude: 8,
+        },
+    ],
+}
