276ac2d3b62e136d5bd30602cd9a23fd4b11c3a6
Philip Schleihauf <uniphil@gmail.com>
2012-06-13 16:02:38 -0400

    Create a simple script to play with pyrtm

--- exscript.py
+++ exscript.py
@@ -1,9 +1,11 @@
 from pyrtm import rtm
 
+print "\nTest 1: a single run of SBdart"
 sbdart = rtm.SBdart()
 myresult = sbdart()
 print myresult
 
+print "\nTest 2: ALL THE RTMs"
 every_rtm = rtm.All()
 all_results = every_rtm()
 print all_results
