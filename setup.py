2fd5ee00d750e222cd1ce5ab8e4f5d9ac63ddea2
Philip Schleihauf <uniphil@gmail.com>
2012-09-13 14:39:10 -0400

    fix readme inclusion

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from setuptools.command.install import install as DistutilsInstall
 
 setup(
     name='RTM',
-    version='0.2.2',
+    version='0.2.3',
     author='Philip Schleihauf',
     author_email='uniphil@gmail.com',
     license='license.txt',
