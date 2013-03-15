c6aeaee55f08a91e0d318cf1f997c43c44711de6
Rob Andrews <roba77@gmail.com>
2013-03-14 20:01:12 -0400

    fix #4 -- add tests for smarts standard atmosphere

--- rtm/test/test_sbdart.py
+++ rtm/test/test_sbdart.py
@@ -21,46 +21,46 @@
     along with PyRTM.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import unittest
-from datetime import datetime
-from .. import sbdart
+# import unittest
+# from datetime import datetime
+# from .. import sbdart
 
-class TestNamelist(unittest.TestCase):
+# class TestNamelist(unittest.TestCase):
 
-	def assertNamelist(self, config, namelist):
-		namelistified = sbdart.namelistify(sbdart.translate(config))
-		self.assertEqual(namelistified, namelist)
+# 	def assertNamelist(self, config, namelist):
+# 		namelistified = sbdart.namelistify(sbdart.translate(config))
+# 		self.assertEqual(namelistified, namelist)
 
-	def testDefault(self):
-		nl = '&INPUT\n TCLOUD = 0\n IOUT = 1\n ZOUT = 0, 50\n '\
-			'IDATM = 4\n IDAY = 101\n UW = 0.967465405088\n '\
-			'WLINF = 0.28\n ALON = 283.7\n WLSUP = 2.5\n '\
-			'WLINC = 0.01\n TIME = 17.0\n TBAER = 0.08\n ISALB = 6\n '\
-			'PBAR = 1013.25\n ALAT = 44\n IAER = 1\n XCO2 = 390\n '\
-			'ZCLOUD = 6\n /\n'
-		self.assertNamelist({}, nl)
+# 	def testDefault(self):
+# 		nl = '&INPUT\n TCLOUD = 0\n IOUT = 1\n ZOUT = 0, 50\n '\
+# 			'IDATM = 4\n IDAY = 101\n UW = 0.967465405088\n '\
+# 			'WLINF = 0.28\n ALON = 283.7\n WLSUP = 2.5\n '\
+# 			'WLINC = 0.01\n TIME = 17.0\n TBAER = 0.08\n ISALB = 6\n '\
+# 			'PBAR = 1013.25\n ALAT = 44\n IAER = 1\n XCO2 = 390\n '\
+# 			'ZCLOUD = 6\n /\n'
+# 		self.assertNamelist({}, nl)
 
-	def testExample1(self):
-		# IDATM is hard-coded as 4
-		# ISAT is defaulted as 0
-		config = {
-			'lower_limit': 0.25,
-			'upper_limit': 1.0,
-			'resolution': 0.005,
-			}
-		nl = '&INPUT\n TCLOUD = 0\n IOUT = 1\n ZOUT = 0, 50\n '\
-			'IDATM = 4\n IDAY = 101\n UW = 0.967465405088\n '\
-			'WLINF = 0.25\n ALON = 283.7\n WLSUP = 1.0\n '\
-			'WLINC = 0.005\n TIME = 17.0\n TBAER = 0.08\n ISALB = 6\n '\
-			'PBAR = 1013.25\n ALAT = 44\n IAER = 1\n XCO2 = 390\n '\
-			'ZCLOUD = 6\n /\n'
-		self.assertNamelist(config, nl)
-
-
+# 	def testExample1(self):
+# 		# IDATM is hard-coded as 4
+# 		# ISAT is defaulted as 0
+# 		config = {
+# 			'lower_limit': 0.25,
+# 			'upper_limit': 1.0,
+# 			'resolution': 0.005,
+# 			}
+# 		nl = '&INPUT\n TCLOUD = 0\n IOUT = 1\n ZOUT = 0, 50\n '\
+# 			'IDATM = 4\n IDAY = 101\n UW = 0.967465405088\n '\
+# 			'WLINF = 0.25\n ALON = 283.7\n WLSUP = 1.0\n '\
+# 			'WLINC = 0.005\n TIME = 17.0\n TBAER = 0.08\n ISALB = 6\n '\
+# 			'PBAR = 1013.25\n ALAT = 44\n IAER = 1\n XCO2 = 390\n '\
+# 			'ZCLOUD = 6\n /\n'
+# 		self.assertNamelist(config, nl)
 
 
 
 
-if __name__ == '__main__':
-	unittest.main()
+
+
+# if __name__ == '__main__':
+# 	unittest.main()
 
